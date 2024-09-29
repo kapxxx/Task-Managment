eac0e874e60a84cdbaec2a1dd840cb32e7587c96
Kapxx <kalpeshprajapati1234i@gmail.com>
2024-09-29 11:11:24 +0530

    remove temp file

---
+++ Repository/Data/MyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Data
+{
+	public class MyDbContext :IdentityDbContext<ApplicationUser>
+	{
+		public MyDbContext(DbContextOptions<MyDbContext> options)
+		: base(options) { }
+
+		public DbSet<Tasks> Tasks { get; set; }
+	//	public DbSet<Comments> Comments { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<ApplicationUser>()
+		   .HasMany(u => u.Tasks) // A user can have many tasks
+		   .WithOne(t => t.CreatedBy) // Each task is created by one user
+		   .HasForeignKey(t => t.CreatedByUserId) // Foreign key in Tasks table
+		   .OnDelete(DeleteBehavior.Restrict); // 
+
+		}
+	}
+}
